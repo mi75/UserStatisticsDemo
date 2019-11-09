79e3d2f2c50382ec4fdecbbfc8b499bc4eedf9d8
mi77 <mi77pilot@gmail.com>
2019-11-09 19:55:26 +0200

    Add user's statistics

--- UserStatisticsDemo/src/app/users-statistic/users-statistic.component.ts
+++ UserStatisticsDemo/src/app/users-statistic/users-statistic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiCallerService } from '../_services/api-caller.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users-statistic',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersStatisticComponent implements OnInit {
 
-  constructor() { }
+  visits:any[];
+
+  constructor(
+    private toServer: ApiCallerService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+
+    let selectedUser = this.route.snapshot.paramMap.get('userId');
+    this.toServer.getData('api/userstat?userId=' + selectedUser).subscribe( 
+      res => this.visits = res,
+      error => alert(error)
+    );
   }
 
 }
