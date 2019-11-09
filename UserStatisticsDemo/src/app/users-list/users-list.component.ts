46b6528e10605e63ef74f1688b6903fcd7252c85
mi77 <mi77pilot@gmail.com>
2019-11-09 23:15:30 +0200

    Paginate in component

--- UserStatisticsDemo/src/app/users-list/users-list.component.ts
+++ UserStatisticsDemo/src/app/users-list/users-list.component.ts
@@ -10,17 +10,39 @@ import { usersFormat } from '../users-format';
 export class UsersListComponent implements OnInit {
 
   users:usersFormat[];
+  startRow:number = 0;
+  rowsCounter:number = 0;
 
   constructor(
     private toServer: ApiCallerService
   ) { }
 
   ngOnInit() {
-    this.toServer.getData('api/userslist')
-    .subscribe(
-      res => this.users = res,
-      error => alert(error)
-    );
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.toServer.getData('api/userslist?startRow=0').subscribe( res => {
+      this.users = res.rows;
+      this.rowsCounter = res.count;
+    } );
+    this.startRow = 0;
+  }
+
+  fbForwarding() {
+    this.startRow += 50;
+    if (this.startRow > this.rowsCounter - 50) this.startRow = this.rowsCounter - 50;
+    this.toServer.getData('api/userslist?startRow=' + this.startRow).subscribe( res => {
+      this.users = res.rows;
+    } );
+  }
+
+  fbReverse() {
+    this.startRow -= 50;
+    if (this.startRow < 0) this.startRow = 0;
+    this.toServer.getData('api/userslist?startRow=' + this.startRow).subscribe( res => {
+      this.users = res.rows;
+    } );
   }
 
 }
